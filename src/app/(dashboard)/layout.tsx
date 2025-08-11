2f686cf683292154ab690ccf6f9f00252f9f9e27
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-08-11 23:26:43 +0530

    Styling

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
-                <div className="w-fit items-center gap-2 my-2">
+                <div className="w-fit items-center gap-2 px-4 my-4">
                     <SidebarTrigger className="-ml-1" />
                 </div>
                 <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
