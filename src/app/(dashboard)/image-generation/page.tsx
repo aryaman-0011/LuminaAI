91a747a92911cb1092bb308fc474746d45c1a9a7
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-08-11 15:58:54 +0530

    Added some styles

--- src/app/(dashboard)/image-generation/page.tsx
+++ src/app/(dashboard)/image-generation/page.tsx
@@ -6,7 +6,7 @@ const ImageGeneration = () => {
     return (
         <section className='container mx-auto grid gap-4 grid-cols-3 overflow-hidden'>
             <Configurations />
-            <div className='col-span-2 p-4 rounded-xl flex items-center justify-center'>
+            <div className='col-span-2 p-4 rounded-xl flex items-center justify-center h-fit'>
                 <GeneratedImages />
             </div>
         </section>
